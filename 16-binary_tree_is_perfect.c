bf4de6a40614e6152ea24b49a1612cc142174f35
wiseman-umanah <wisemanumanah@gmail.com>
2024-01-31 13:47:59 +0100

    fixed code

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -3,35 +3,48 @@
 /**
  * binary_tree_depth - returns the depth of the tree
  * @node:  node to start from
- * Returns: the depth
+ * Return: the depth
 */
 size_t binary_tree_depth(const binary_tree_t *node)
 {
-    int depth = 0;
-    while (node != NULL)
+	int depth = 0;
+
+	while (node != NULL)
 	{
-        depth++;
-        node = node->left;
-    }
-    return depth;
+		depth++;
+		node = node->left;
+	}
+	return (depth);
 }
 
-
+/**
+ * is_perfect - checks if a binary tree is perfect or not
+ * @tree: the tree to use
+ * @depth: the depth
+ * @level: the level
+ * Return: the depth
+*/
 int is_perfect(const binary_tree_t *tree, int depth, int level)
 {
-    if (tree == NULL)
-        return 1;
-    if (tree->left == NULL && tree->right == NULL)
-        return (depth == level + 1);
-    if (tree->left == NULL || tree->right == NULL)
-        return 0;
-    return is_perfect(tree->left, depth, level + 1) &&
-           is_perfect(tree->right, depth, level + 1);
+	if (tree == NULL)
+		return (1);
+	if (tree->left == NULL && tree->right == NULL)
+		return (depth == level + 1);
+	if (tree->left == NULL || tree->right == NULL)
+		return (0);
+	return (is_perfect(tree->left, depth, level + 1) &&
+		   is_perfect(tree->right, depth, level + 1));
 }
 
+/**
+ * binary_tree_is_perfect - 0 is not perfect
+ * @tree: the tree
+ * Return: 1 if true, 0 if false
+*/
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    int d = binary_tree_depth(tree);
-    return is_perfect(tree, d, 0);
+	int d = binary_tree_depth(tree);
+
+	return (is_perfect((tree, d, 0)));
 }
