d51db103ee1a0c57fc4cb89ef4501593f5b7aedc
wiseman-umanah <wisemanumanah@gmail.com>
2024-01-31 14:13:38 +0100

    Fixed code

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,39 +1,17 @@
 #include "binary_trees.h"
 
 
-/**
- * num_of_elem - calculate the number of nodes in tree
- * @tree: the tree to use
- * Return: the size of the tree
-*/
-
-double num_of_elem(const binary_tree_t *tree)
-{
-	int num;
-
-	if (tree == NULL)
-		return (0);
-	num = 0;
-	num += num_of_elem(tree->left);
-	num += num_of_elem(tree->right);
-	num++;
-	return (num);
-}
-
-/**
- * binary_tree_height - calculate the height of a tree
- * @tree: the tree to use
- * Return: the height of the tree
-*/
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height;
-	int num;
+	size_t left_height, right_height;
 
-	if (tree == NULL)
-		return (0);
-	num = num_of_elem(tree);
-	height = log2((double)num);
-	return (height);
+	left_height = right_height = 0;
+    if (tree == NULL) {
+        return 0; 
+    }
+
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
+
+    return (left_height > right_height ? left_height : right_height) + 1;
 }
